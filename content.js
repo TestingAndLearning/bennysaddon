0fff581d7c5d8f1202026744bfd1a3883e6ff00a
mt9304 <mt9304@gmail.com>
2018-07-19 11:35:54 -0700

    Made external script.

--- content.js
+++ content.js
@@ -1,4 +1,22 @@
-//The addon makes the text box height bigger everytime you click it.
-var textBoxHeight = document.querySelectorAll('[title="HTML Body"]')[0].offsetHeight;
-var textBox = document.querySelectorAll('[title="HTML Body"]')[0];
-document.querySelectorAll('[title="HTML Body"]')[0].style.setProperty('height', textBoxHeight+150+'px','');
+if (!scriptIsLoaded)
+{
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.onload = function() {
+	    callFunctionFromScript();
+	}
+	script.src = 'https://raw.githubusercontent.com/mt9304/bennysaddon/master/mainfunction.js';
+	head.appendChild(script);
+}
+
+increaseTextBoxHeight();
+
+function scriptIsLoaded(url) {
+    if (!url) url = "https://raw.githubusercontent.com/mt9304/bennysaddon/master/mainfunction.js";
+    var scripts = document.getElementsByTagName('script');
+    for (var i = scripts.length; i--;) {
+        if (scripts[i].src == url) return true;
+    }
+    return false;
+}
---
+++ mainfunctions.js
@@ -0,0 +1,6 @@
+function increaseTextBoxHeight() {
+  //The addon makes the text box height bigger everytime you click it.
+  var textBoxHeight = document.querySelectorAll('[title="HTML Body"]')[0].offsetHeight;
+  var textBox = document.querySelectorAll('[title="HTML Body"]')[0];
+  document.querySelectorAll('[title="HTML Body"]')[0].style.setProperty('height', textBoxHeight+150+'px','');
+}
