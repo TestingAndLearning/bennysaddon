1cd70436eacc3a892994551d061b3ea5c9783bf2
mt9304 <mt9304@gmail.com>
2018-07-19 11:48:56 -0700

    Reverted back to working version.

--- mainfunctions.js
+++ mainfunctions.js
@@ -4,3 +4,30 @@ function increaseTextBoxHeight() {
   var textBox = document.querySelectorAll('[title="HTML Body"]')[0];
   document.querySelectorAll('[title="HTML Body"]')[0].style.setProperty('height', textBoxHeight+150+'px','');
 }
+/*
+
+if (!scriptIsLoaded())
+{
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.onload = function() {
+	    callFunctionFromScript();
+	}
+	script.src = 'https://rawgit.com/mt9304/bennysaddon/master/mainfunctions.js';
+	head.appendChild(script);
+}
+
+increaseTextBoxHeight();
+
+function scriptIsLoaded() {
+    url = "https://rawgit.com/mt9304/bennysaddon/master/mainfunctions.js";
+    var scripts = document.getElementsByTagName('script');
+    for (var i = scripts.length; i--;) {
+        if (scripts[i].src == url) return true;
+    }
+    return false;
+}
+
+
+*/
